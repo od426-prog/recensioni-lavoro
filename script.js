11b3a423fcf6b427b071d4a51d8c763dcba6825b
od426-prog <oscardambrosio426@gmail.com>
2026-08-14 15:53:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -231,17 +231,17 @@ reviewsList.innerHTML = reviews.map(review => {
     "★".repeat(review.valutazione) +
     "☆".repeat(5 - review.valutazione);
 
-  const date = review.created_at
-    ? new Date(review.created_at).toLocaleDateString("it-IT")
-    : "";
+const date = review.created_at
+  ? new Date(review.created_at).toLocaleDateString("it-IT")
+  : "Data non disponibile";
 
   return `
     <div class="review-modal-item">
 
-      <div class="review-modal-header">
-        <strong>UTENTE ANONIMO</strong>
-        <span>${date}</span>
-      </div>
+<div class="review-modal-header">
+  <strong>UTENTE ANONIMO</strong>
+  <span>Pubblicata il ${date}</span>
+</div>
 
       <div class="review-view-rating">
         ${stars}
